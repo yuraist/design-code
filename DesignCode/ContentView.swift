fd917547507f1c7b2c54a968345afb3d2d992ad8
Yura Istomin <yura.ist@icloud.com>
2019-10-11 18:23:44 +0300

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Yura Istomin on 11.10.2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
