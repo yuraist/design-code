7246196a31e4865a9bad1778ad3b69067016d087
Yura Istomin <yura.ist@icloud.com>
2019-10-11 18:43:23 +0300

    initial setup

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,13 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
+  var body: some View {
+    VStack {
+      VStack {
+        Text("UI Design")
+          .font(.largeTitle)
+          .fontWeight(.bold)
+          .foregroundColor(Color.purple)
+          .padding(.top)
+        Text("Certificate")
+          .foregroundColor(.white)
+      }
+      Image("Background")
     }
+    .background(Color.black)
+    .cornerRadius(15)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
