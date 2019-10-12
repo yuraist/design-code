f6145a062f1ac7462ffee8b89372417ff934b504
Yura Istomin <yura.ist@icloud.com>
2019-10-12 12:14:38 +0300

    extract subviews

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -11,40 +11,13 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     ZStack {
-      VStack {
-        Text("Placeholder")
-      }
-      .frame(width: 300, height: 220)
-      .background(Color.blue)
-      .cornerRadius(15)
-      .shadow(radius: 20)
-      .offset(x: 0, y: -20)
+      CardView()
+        .offset(x: 0, y: -40)
       
-      VStack {
-        HStack {
-          VStack(alignment: .leading) {
-            Text("UI Design")
-              .font(.headline)
-              .fontWeight(.bold)
-              .foregroundColor(Color("accent"))
-              .padding(.top)
-            Text("Certificate")
-              .font(.body)
-              .foregroundColor(.white)
-          }
-          Spacer()
-          Image("Logo")
-            .resizable()
-            .frame(width: 30.0, height: 30.0)
-        }
-        .padding(.horizontal)
-        Spacer()
-        Image("Background")
-      }
-      .frame(width: 340.0, height: 220)
-      .background(Color.black)
-      .cornerRadius(15)
-      .shadow(radius: 20)
+      CardView()
+        .offset(x: 0, y: -20)
+      
+      CertificateView()
     }
   }
 }
@@ -52,5 +25,47 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
+  }
+}
+
+struct CardView: View {
+  var body: some View {
+    VStack {
+      Text("Placeholder")
+    }
+    .frame(width: 340, height: 220)
+    .background(Color.blue)
+    .cornerRadius(15)
+    .shadow(radius: 20)
+  }
+}
+
+struct CertificateView: View {
+  var body: some View {
+    VStack {
+      HStack {
+        VStack(alignment: .leading) {
+          Text("UI Design")
+            .font(.headline)
+            .fontWeight(.bold)
+            .foregroundColor(Color("accent"))
+            .padding(.top)
+          Text("Certificate")
+            .font(.body)
+            .foregroundColor(.white)
+        }
+        Spacer()
+        Image("Logo")
+          .resizable()
+          .frame(width: 30.0, height: 30.0)
+      }
+      .padding(.horizontal)
+      Spacer()
+      Image("Background")
+    }
+    .frame(width: 340.0, height: 220)
+    .background(Color.black)
+    .cornerRadius(15)
+    .shadow(radius: 20)
   }
 }
