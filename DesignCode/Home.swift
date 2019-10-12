91e8604ab955585e5fedd3f70b355386483d2878
Yura Istomin <yura.ist@icloud.com>
2019-10-12 16:41:29 +0300

    show CertificateView on the Home screen

--- DesignCode/Home.swift
+++ DesignCode/Home.swift
@@ -11,15 +11,21 @@ import SwiftUI
 struct Home: View {
   
   @State var show = false
+  @State var showProfile = false
   
   var body: some View {
     
     ZStack {
-      Button(action: {
-        self.show.toggle()
-      }) {
-        Text("Open menu")
-      }
+      ContentView()
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .animation(.spring())
+        .offset(y: showProfile ? 40 : UIScreen.main.bounds.height)
+      
+      TopMenuButtons(show: $show, showProfile: $showProfile)
+        .animation(.spring())
+        .offset(x: -30, y: showProfile ? 0 : 80)
       
       MenuView(show: $show)
     }
@@ -89,6 +95,83 @@ struct MenuView: View {
     .offset(x: show ? 0 : -UIScreen.main.bounds.width)
     .onTapGesture {
       self.show.toggle()
+    }
+  }
+}
+
+struct CircleButton: View {
+  
+  var icon = "person.crop.circle"
+  
+  var body: some View {
+    HStack {
+      Image(systemName: icon)
+        .foregroundColor(.black)
+    }
+    .frame(width: 44, height: 44)
+    .background(Color.white)
+    .cornerRadius(30)
+    .shadow(color: Color("buttonShadow"),
+            radius: 10, x: 0, y: 10)
+  }
+}
+
+struct MenuButton: View {
+  
+  @Binding var show: Bool
+  
+  var body: some View {
+    Button(action: {
+      self.show.toggle()
+    }) {
+      HStack {
+        Spacer()
+        Image(systemName: "list.dash")
+          .foregroundColor(.black)
+      }
+      .padding(.trailing, 20)
+      .frame(width: 90, height: 60)
+      .background(Color.white)
+      .cornerRadius(30)
+      .shadow(color: Color("buttonShadow"),
+              radius: 10, x: 0, y: 10)
+    }
+  }
+}
+
+struct MenuRight: View {
+  
+  @Binding var show: Bool
+  
+  var body: some View {
+    HStack {
+      Spacer()
+      Button(action: { self.show.toggle() }) {
+        CircleButton()
+      }
+      Button(action: { self.show.toggle() }) {
+        CircleButton(icon: "bell")
+      }
+    }
+    .offset(x: 15)
+  }
+}
+
+struct TopMenuButtons: View {
+  
+  @Binding var show: Bool
+  @Binding var showProfile: Bool
+  
+  var body: some View {
+    HStack {
+      VStack {
+        HStack {
+          MenuButton(show: $show)
+          MenuRight(show: $showProfile)
+        }
+        
+        Spacer()
+      }
     }
   }
 }
